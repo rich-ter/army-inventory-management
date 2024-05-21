2ce1b62b0171d0584b3ff789a753447c5d8ccbd1
Richter <me@philipposrichter.com>
2024-05-21 10:18:44 +0300

    creating docker

--- hud_django_v1.0/template_django/DjangoHUD/settings.py
+++ hud_django_v1.0/template_django/DjangoHUD/settings.py
@@ -38,8 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-
 ]
 
 MIDDLEWARE = [
@@ -123,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
