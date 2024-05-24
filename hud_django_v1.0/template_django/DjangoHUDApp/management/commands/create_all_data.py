dc6e2fa6d6b27168270c62bc5ca096ff7e061932
Richter <me@philipposrichter.com>
2024-05-24 13:33:26 +0300

    adding master managemetn command

---
+++ hud_django_v1.0/template_django/DjangoHUDApp/management/commands/create_all_data.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.core.management import call_command
+
+class Command(BaseCommand):
+    help = 'Runs all the specified management commands in order'
+
+    def handle(self, *args, **options):
+        commands = [
+            'create_final_recipients',
+            'create_groups_and_users',
+            'create_doriforika_products',
+            'create_kepik_products',
+            'create_kepik_products_tsaprou'
+        ]
+
+        for command in commands:
+            self.stdout.write(f'Running {command}...')
+            try:
+                call_command(command)
+                self.stdout.write(self.style.SUCCESS(f'Successfully ran {command}'))
+            except CommandError as e:
+                self.stdout.write(self.style.ERROR(f'Error running {command}: {e}'))
+                raise CommandError(f'Error running {command}: {e}')
+
+        self.stdout.write(self.style.SUCCESS('All commands have been executed successfully'))
